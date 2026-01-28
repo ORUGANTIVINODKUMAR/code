6f4461996f6f3cbf5e4eedb80290292e2d2d1318
ORUGANTIVINODKUMAR <144659708+ORUGANTIVINODKUMAR@users.noreply.github.com>
2026-01-28 12:50:19 +0530

    Update Signup.jsx

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -39,7 +39,10 @@ const Signup = () => {
     try {
       setLoading(true);
 
-      const res = await fetch("http://localhost:5000/api/auth/signup", {
+      const API_URL = process.env.REACT_APP_API_URL;
+
+      const res = await fetch(`${API_URL}/api/auth/signup`, {
+
         method: "POST",
         headers: {
           "Content-Type": "application/json"
